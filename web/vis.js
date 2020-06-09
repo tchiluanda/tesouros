3c836997062906be06874d3e2d425f44ebcd8498
tiagombp <tiagombp@gmail.com>
2020-06-09 18:54:32 -0300

    implementa código para distribuir bolinhas conforme valores. faz o primeiro.

--- web/vis.js
+++ web/vis.js
@@ -44,7 +44,8 @@ function insere_frases() {
         .classed("frases", true)
         .style("opacity", 0)
         .append("span")
-        .text(texto => "●" + ' "' + texto + '"');
+        .text(texto => '"' + texto + '"');
+        //.text(texto => "●" + ' "' + texto + '"');
 
     //$frases = $cont_svg.selectAll("p.frases");
 }
@@ -135,33 +136,74 @@ function prepara_dados(dados, criterio, raio, margem) {
     // determina valores únicos
     let dados_sumarizados = sumariza_dados(dados, criterio);
 
-    let contagem_maxima = d3.max(dados_sumarizados);
+    let contagem_maxima = d3.max(dados_sumarizados, d => d.contagem);
 
     let qde_linhas_grid = Math.ceil(Math.sqrt(contagem_maxima));
 
-    let qde_colunas_grid = dados_sumarizados.map(d => Math.ceil(d.contagem/qde_linhas_grid));
+    let parametros_colunas_grid = dados_sumarizados.map(d => {
+        qde = Math.ceil(d.contagem/qde_linhas_grid);
+        return({ 
+            "categoria" : d.categoria,
+            "largura" : qde * 3 * raio
+          }
+        )
+        });
 
-    let posicoes_iniciais = qde_colunas_grid.map(d => (d-1) * raio + margem);
+    let pos_inicial_ac = 0;
 
-    let mini_dados = [];
+    let posicoes_iniciais = {};
+    parametros_colunas_grid.forEach((d,i) => {
+        let largura_anterior =
+          i == 0 ? 0 : parametros_colunas_grid[i-1].largura + margem;
+
+        pos_inicial_ac += largura_anterior;
+
+        d["pos_inicial"] = pos_inicial_ac;
+
+        posicoes_iniciais[d.categoria] = d.pos_inicial;
+    });
+    console.log({parametros_colunas_grid});
+    
+    
+
+
+    let mini_dados = {};
+    mini_dados["dados"] = [];
     let categorias = dados_sumarizados.map(d => d.categoria);
-    console.log("Categorias: ", categorias)
+    
     for (cat of categorias) {
         dados
           .filter(d => d[criterio] == cat)
-          .forEach((d,i) => mini_dados.push({
+          .forEach((d,i) => mini_dados.dados.push({
               "nome" : d.nome,
               "categoria" : d[criterio], // que é próprio cat
-              "index_relativo" : i
+              "x_ini" : d["x_ini"],
+              "y_ini" : d["y_ini"],
+              "x" : d.x,
+              "y" : d.y,
+              "value" : d.value,
+              "index_relativo" : i,
+              "eixo_principal" : Math.floor(i / (qde_linhas_grid + 1)) * 3 * raio,
+              "eixo_secundario" : (i % (qde_linhas_grid + 1)) * 3 * raio
           }));
-    }
+    };
+
+    let ultimo_conjunto = parametros_colunas_grid.slice(-1).pop();
+    let tamanho_necessario = ultimo_conjunto.pos_inicial + ultimo_conjunto.largura;
+
+    mini_dados["parametros"] = {
+        "posicoes_iniciais" : posicoes_iniciais,
+        "parametros_coluna" : parametros_colunas_grid,
+        "largura_eixo_principal_total" : tamanho_necessario,
+        "largura_eixo_secundario_total" : qde_linhas_grid * 3 * raio,
+        "resumo" : dados_sumarizados
+    };
+
     return(mini_dados);
 }
 
 d3.csv("./web/dados/data.csv").then(function(grid) {
     console.log(grid[0], grid.columns);
-
-
 
     let margin = w < 580 ? 15 : 25;
 
@@ -215,7 +257,7 @@ d3.csv("./web/dados/data.csv").then(function(grid) {
       .domain(["1", "2", "3"])
       .range(["rgb(255,213,0)", "rgb(0,74,147)", "rgb(50,156,50)"]);
 
-    let pontos = $svg.selectAll("circle.pontos").data(grid);
+    let pontos = $svg.selectAll("circle.pontos").data(grid, d => d.nome);
 
     let pontos_enter = pontos.enter()
       .append("circle")
@@ -228,6 +270,8 @@ d3.csv("./web/dados/data.csv").then(function(grid) {
 
     pontos = pontos.merge(pontos_enter);
 
+    //console.log("Pontos antes", pontos)
+
     insere_frases();
 
     function desenha(step, direcao) {
@@ -238,6 +282,9 @@ d3.csv("./web/dados/data.csv").then(function(grid) {
             case 2 :
                 desenha_step2(direcao);
                 break;
+            case 3 :
+                desenha_step3(direcao);
+                break;                
         }
     }
 
@@ -272,31 +319,71 @@ d3.csv("./web/dados/data.csv").then(function(grid) {
     }
 
     function desenha_step2(direcao) {
-        if (!flag) {
-            t.stop();
-            flag = true;
-        }
+        if (direcao == "descendo") {
+            if (!flag) {
+                t.stop();
+                flag = true;
+            }
+    
+            d3.selectAll("p.frases")
+              .transition()
+              .duration(duracao)
+              .style("opacity", 0);
+    
+            d3.selectAll("h1")
+              .transition()
+              .duration(duracao/2)
+              .style("opacity", 0);       
+    
+            pontos.transition()
+              .duration(d => Math.random() * duracao)
+              .attr("opacity", 1);
+             
+    
+            pontos.transition()
+              .delay(duracao)
+              .duration(duracao)
+              .attr("cx", d => x(d.x))
+              .attr("cy", d => y(d.y));
 
-        d3.selectAll("p.frases")
+        } else {
+            
+            pontos.transition()
+                .duration(duracao)
+                .attr("fill", d => cor_inicial(d.value))
+                .attr("cx", d => x(d.x))
+                .attr("cy", d => y(d.y));
+        }       
+
+    }
+
+    function desenha_step3(direcao) {
+
+        console.log("ok, tô no step 3");
+        let mini_dados = prepara_dados(grid, "1. Idade", raio, margin);
+
+        let largura_total = mini_dados.parametros.largura_eixo_principal_total;
+        let altura_total = mini_dados.parametros.largura_eixo_secundario_total;
+
+        let margem_inicial_principal = (w - largura_total)/2;
+        let margem_inicial_secundario = (h - altura_total)/2;
+
+        let cor = d3.scaleOrdinal()
+          .domain(mini_dados.parametros.resumo.map(d => d.categoria))
+          .range(d3.schemeCategory10)
+
+        //let pontos = d3.selectAll("circle.pontos")
+        pontos = pontos
+          .data(mini_dados.dados, d => d.nome);
+
+        console.log("Pontos", pontos, pontos.enter(), pontos.exit());
+
+        pontos
           .transition()
           .duration(duracao)
-          .style("opacity", 0);
-
-        d3.selectAll("h1")
-          .transition()
-          .duration(duracao/2)
-          .style("opacity", 0);       
-
-        pontos.transition()
-          .duration(d => Math.random() * duracao)
-          .attr("opacity", 1);
-         
-
-        pontos.transition()
-          .delay(duracao)
-          .duration(duracao)
-          .attr("cx", d => x(d.x))
-          .attr("cy", d => y(d.y));
+          .attr("cx", d => d.eixo_principal + margem_inicial_principal + mini_dados.parametros.posicoes_iniciais[d.categoria])
+          .attr("cy", d => d.eixo_secundario + margem_inicial_secundario)
+          .attr("fill", d => cor(d.categoria));        
     }
 
     // setup
@@ -312,7 +399,7 @@ d3.csv("./web/dados/data.csv").then(function(grid) {
             console.log(steps);
             console.log("avançando");
 
-            desenha(step, "voltando");
+            desenha(step, "descendo");
         },
 
         exit: function(el) {
