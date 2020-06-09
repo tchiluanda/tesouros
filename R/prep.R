5932bdc8a033258084255dfcb6790e7aebce5d05
tiagombp <tiagombp@gmail.com>
2020-06-09 17:00:40 -0300

    corrige geração de csv para a visualização

--- R/prep.R
+++ R/prep.R
@@ -6,13 +6,17 @@ dados_raw <- read_excel("./R/dados/Diálogos estratégicos Final 0806.xlsx", ski
 
 grid <- read_rds("./R/dados/grid.rds")
 
-dados <- bind_cols(dados_raw, grid)
+dados <- 
 
-dados %>% ggplot(aes(`1. Idade`)) + geom_histogram(stat="count")
-dados %>% ggplot(aes(`2. Sexo`)) + geom_histogram(stat="count")
-dados %>% ggplot(aes(`3. Escolaridade`)) + geom_histogram(stat="count")
-dados %>% ggplot(aes(`6. Tempo total no Tesouro`)) + geom_histogram(stat="count")
-dados %>% ggplot(aes(`9. Qual a sua unidade de lotação atual?`)) + geom_histogram(stat="count")
-dados %>% ggplot(aes(`1. Você está satisfeito com a sua situação atual no Tesouro?`)) + geom_histogram(stat="count")
+dados_raw %>% ggplot(aes(`1. Idade`)) + geom_histogram(stat="count")
+dados_raw %>% ggplot(aes(`2. Sexo`)) + geom_histogram(stat="count")
+dados_raw %>% ggplot(aes(`3. Escolaridade`)) + geom_histogram(stat="count")
+dados_raw %>% ggplot(aes(`6. Tempo total no Tesouro`)) + geom_histogram(stat="count")
+dados_raw %>% ggplot(aes(`9. Qual a sua unidade de lotação atual?`)) + geom_histogram(stat="count")
+dados_raw %>% ggplot(aes(`1. Você está satisfeito com a sua situação atual no Tesouro?`)) + geom_histogram(stat="count")
 
-dados %>% select(1:11) %>% write.csv(file = "./web/dados/data.csv", fileEncoding = "UTF-8")
+dados <- dados_raw %>% 
+  select(1:11) %>% 
+  bind_cols(grid)
+
+dados %>% write.csv(file = "./web/dados/data.csv", fileEncoding = "UTF-8")
