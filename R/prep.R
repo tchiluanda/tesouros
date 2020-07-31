62f5a32d1479b95fce946dd3df1518039651c06f
tiagombp <tiagombp@gmail.com>
2020-07-31 13:33:33 -0300

    update

--- R/prep.R
+++ R/prep.R
@@ -1,12 +1,11 @@
 library(tidyverse)
 library(readxl)
 library(skimr)
+library(colorspace)
 
 dados_raw <- read_excel("./R/dados/Diálogos estratégicos Final 0806.xlsx", skip = 1)
 
 grid <- read_rds("./R/dados/grid.rds")
-
-dados <- 
 
 dados_raw %>% ggplot(aes(`1. Idade`)) + geom_histogram(stat="count")
 dados_raw %>% ggplot(aes(`2. Sexo`)) + geom_histogram(stat="count")
@@ -15,8 +14,71 @@ dados_raw %>% ggplot(aes(`6. Tempo total no Tesouro`)) + geom_histogram(stat="co
 dados_raw %>% ggplot(aes(`9. Qual a sua unidade de lotação atual?`)) + geom_histogram(stat="count")
 dados_raw %>% ggplot(aes(`1. Você está satisfeito com a sua situação atual no Tesouro?`)) + geom_histogram(stat="count")
 
+dados_satisfacao <- dados_raw %>%
+  select(idade = `1. Idade`, 
+         satis = `1. Você está satisfeito com a sua situação atual no Tesouro?`) %>%
+  mutate(satis = factor(satis, levels = rev(c("Não", "Possivelmente não", "Sinto-me indiferente", "Basicamente sim", "Sim")), ordered = T))
+
+dados_satisfacao_plot <- dados_satisfacao %>%
+  count(idade,  satis)
+
+ggplot(dados_satisfacao_plot, aes(fill = satis, y = idade, x = n)) +
+  geom_col(position = position_fill()) +
+  scale_fill_discrete_diverging()
+
 dados <- dados_raw %>% 
-  select(1:11) %>% 
+  select(1:12) %>% 
   bind_cols(grid)
 
 dados %>% write.csv(file = "./web/dados/data.csv", fileEncoding = "UTF-8")
+
+
+# misc --------------------------------------------------------------------
+
+dput(unique(dados_raw$`1. Idade`))
+dput(unique(dados_raw$`3. Escolaridade`))
+dput(unique(dados_raw$`1. Você está satisfeito com a sua situação atual no Tesouro?`))
+dput(unique(unlist(dados_raw[,10])))
+
+tab_unidades <- data.frame(
+  "unidades" = c(
+    "ASSEC", 
+    "Gabinete",
+    "CCONT", 
+    "CCONF", 
+    "COINC", 
+    "CODIN",
+    "COSIS",
+    "COFIN", 
+    "COPAR", 
+    "CODIP", 
+    "CODIV",
+    "COGEP",
+    
+ 
+
+    "COPEF", 
+    "CFORM", 
+    "COREM", 
+
+
+    "CGFIS", 
+ 
+    "COGEF", 
+    "COAFI", "CORIS", "CESEF", "COPEM", "CPLAN", 
+    "COINT", "Unidade fora do Tesouro"
+  ),
+  "subsec" = c(
+    "Gabinete",
+    "Gabinete",
+    "Sucon",
+    "Sucon",
+    "Sucon",
+    "Sucop",
+    "Sucop",
+    "Sudip",
+    "Sudip",
+    "Sudip",
+    
+    
+  ))
